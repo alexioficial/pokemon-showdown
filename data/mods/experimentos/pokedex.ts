c73403fb59345ba90756b221f4403fc02466ab6f
alexioficial <grullonmatiasalexi@gmail.com>
2025-11-24 12:55:45 -0400

    arreglando

--- data/mods/experimentos/pokedex.ts
+++ data/mods/experimentos/pokedex.ts
@@ -2,14 +2,13 @@ export const Pokedex: import('../../../sim/dex-species').ModdedSpeciesDataTable 
 	gojo: {
 		num: -1,
 		name: "Gojo",
-		baseSpecies: "Magikarp", // Usa el sprite de Magikarp
 		types: ["Psychic", "Fighting"],
 		baseStats: { hp: 105, atk: 90, def: 110, spa: 165, spd: 160, spe: 150 },
 		abilities: { 0: "Limitless" },
 		heightm: 1.9,
 		weightkg: 84.7,
-		color: "Red",
-		eggGroups: ["Monster"],
+		color: "Blue",
+		eggGroups: ["Undiscovered"],
 	},
 	sukuna: {
 		num: -2,
